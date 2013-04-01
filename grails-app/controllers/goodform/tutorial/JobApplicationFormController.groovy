3428bc4b4545dc97401540dd95e2a293cfa196b7
Ross Rowe <piaross@gmail.com>
2013-04-01 16:36:07 +1100

    Added PDF generation logic

--- grails-app/controllers/goodform/tutorial/JobApplicationFormController.groovy
+++ grails-app/controllers/goodform/tutorial/JobApplicationFormController.groovy
@@ -1,10 +1,27 @@
 package goodform.tutorial
 
 import com.nerderg.goodForm.FormController
+import com.nerderg.goodForm.FormInstance
 
 class JobApplicationFormController extends FormController {
 
+    def pdfRenderingService
+
     def createForm() {
         createForm('JobApplication')
+    }
+
+    @Override
+    def submit(Long id) {
+
+        FormInstance formInstance = formDataService.getFormInstance(id)
+        Map formData = formInstance.storedFormData()
+        File location = new File(grailsApplication.config.goodform.uploaded.file.location.toString() + '/form/' + id)
+        location.mkdirs()
+        File upload = new File(location, "applicationForm.pdf")
+        upload.withOutputStream {outputStream ->
+            pdfRenderingService.render([template: '/jobApplication/view', model: [formInstance: formInstance, formData: formData]], outputStream)
+        }
+        return super.submit(id)
     }
 }
